4d735dc40904289b04b0f1cbe28bd38bc05d1e9c
ASUS <cam763141@gmail.com>
2026-05-13 00:16:29 +0700

    Add Firebase to project and implement Splash Activity

---
+++ app/src/main/java/com/example/ecommerce/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.ecommerce
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ecommerce.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.startBtn.setOnClickListener {
+            startActivity(
+              Intent(
+                this@SplashActivity,
+            MainActivity::class.java
+            )
+        )
+
+        }
+    }
+}
